aaba376f716b00ddd44a4ab65147eadc46e208af
nilshe98-create <nilshe98@gmail.com>
2026-05-04 16:20:46 +0800

    Create vedastro.js

---
+++ api/vedastro.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).end();
+
+  const { time, location } = req.query;
+
+  try {
+    const vedTime = decodeURIComponent(time);
+    const vedLocation = decodeURIComponent(location);
+    const url = `https://api.vedastro.org/api/Calculate/AllPlanetData/PlanetName/All/${vedLocation}/${vedTime}?apiKey=${process.env.VEDASTRO_API_KEY}`;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
